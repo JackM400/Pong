2c46e3256a79eb086a8f306f1e6555e166aa9de6
JackM400 <jack.millar400@gmail.com>
2019-08-08 18:49:27 +0100

    left bar created

--- Pong.py
+++ Pong.py
@@ -4,5 +4,19 @@
 
 import turtle
 
+isRunning = True
 gameWindow = turtle.Screen()
 gameWindow.title("Pong - JackM400")
+gameWindow.setup(width=900, height=700)
+gameWindow.bgcolor("black")
+
+leftBar = turtle.Turtle()
+leftBar.speed(0)
+leftBar.shape("square")
+leftBar.color("white")
+leftBar.penup()
+leftBar.goto(-390, 0)
+
+# main function/loop
+while isRunning:
+    gameWindow.update()
