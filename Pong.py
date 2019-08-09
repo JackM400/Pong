e767c9bf06f06e8ceb2760cf159c38a974ca6511
JackM400 <jack.millar400@gmail.com>
2019-08-09 16:34:00 +0100

    pong created and placed on screen

--- Pong.py
+++ Pong.py
@@ -10,6 +10,14 @@ gameWindow.title("Pong - JackM400")
 gameWindow.setup(width=900, height=700)
 gameWindow.bgcolor("black")
 
+# pong
+pong = turtle.Turtle()
+pong.speed(0)
+pong.shape("square")
+pong.color("white")
+pong.penup()
+pong.goto(0, 0)
+
 # left bar
 leftBar = turtle.Turtle()
 leftBar.speed(0)
